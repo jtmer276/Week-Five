29c4810be9dcbe4bda9efede4c4b481a352a52f9
jtmer276 <jtmerrill276@gmail.com>
2024-03-11 14:51:12 -0400

    fewwefwe

--- election.java
+++ election.java
@@ -8,22 +8,31 @@ import java.util.*;
 
  public class election{
      public static void main(String[] args){
-        int awbreyVote;
+        int awbreyVote = 0;
         double awbreyPercentage;
-        int martinezVote;
+        int martinezVote = 0;
         double martinezPercentage;
         int totalVotes;
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number of Awbrey's votes: ");
-        awbreyVote = input.nextInt();
-        System.out.print("Enter the number of Martinez's votes: ");
-        martinezVote = input.nextInt();
+        System.out.print("Enter the number of Awbrey's votes from New Y: ");
+        awbreyVote += input.nextInt();
+        System.out.print("Enter the number of Awbrey's votes from New Jersey: ");
+        awbreyVote += input.nextInt();
+        System.out.print("Enter the number of Awbrey's votes from Connecticut: ");
+        awbreyVote += input.nextInt();
+        System.out.print("Enter the number of Martinez's votes from New York: ");
+        martinezVote += input.nextInt();
+        System.out.print("Enter the number of Martinez's votes from New Jersey: ");
+        martinezVote += input.nextInt();
+        System.out.print("Enter the number of Martinez's votes from Connecticut: ");
+        martinezVote += input.nextInt();
         totalVotes = (martinezVote + awbreyVote);
         awbreyPercentage = awbreyVote/totalVotes;
-        martinezPercentage = (martinezVote/totalVotes)*100;
+        awbreyPercentage = (double)awbreyVote/(double)totalVotes*100;
+        martinezPercentage = (double)martinezVote/(double)totalVotes*100;
         /*System.out.println("Candidate\t\t Votes\t Percentage");
         System.out.println("Awbrey\t\t\t "+ awbreyVote); */
-        System.out.println(totalVotes);
+        System.out.println("\t\t Awbrey");
         System.out.println(awbreyVote);
         System.out.println(awbreyPercentage);
 
