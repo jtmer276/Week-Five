c63549f4ca87b39b7b0c4a013482981b5b56e89c
jtmer276 <159070824+jtmer276@users.noreply.github.com>
2024-03-14 12:34:27 -0400

    complete test

---
+++ building.java
@@ -0,0 +1,30 @@
+/******
+Jules Merrill
+3/14/2023
+Project: Building
+ */
+
+import java.util.*;
+import java.text.NumberFormat;
+
+ public class building{
+     public static void main(String[] args){
+        int poles = 0;
+        double cylinders = 0;
+        double pi = 3.14159;
+        double radius = 1.5;
+        double final_price = 0;
+
+        NumberFormat currency= NumberFormat.getCurrencyInstance();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of the poles: ");
+        poles = input.nextInt();
+
+        cylinders = (double)poles*(pi*radius*radius*5);
+        final_price = (cylinders*27)*12.98;
+        final_price = (float)final_price;
+
+        System.out.println("The final price is: " + currency.format(final_price));
+     }
+
+    }
---
+++ geometry.java
@@ -0,0 +1,38 @@
+/***
+ Jules Merrill
+ 3/14/2024
+ Geometry
+ */
+
+import java.util.*;
+import java.text.NumberFormat;
+
+public class geometry {
+    public static void main(String[] args){
+        int m = 0;
+        int n = 0;
+        int side_one = 0;
+        int side_two = 0;
+        int hypotenuse = 0;
+
+        NumberFormat currency= NumberFormat.getCurrencyInstance();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first greater integer: ");
+        m = input.nextInt();
+        System.out.print("Enter the second lesser integer: ");
+        n = input.nextInt();
+
+        side_one = (m*m)-(n*n);
+        side_two = (2*m)*n;
+        hypotenuse = (m*m)+(n*n);
+
+        System.out.println("Side one: " +(side_one));
+        System.out.println("Side two: " +(side_two));
+        System.out.println("Hypotenuse: " +(hypotenuse));
+
+        double cost = (2.412*(side_one))+(3.767*(side_two))+(15.758*(hypotenuse));
+        System.out.println("The price is: " + currency.format(cost));       
+
+    }
+    
+}
