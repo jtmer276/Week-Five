18e4bae314b45a8befa015abae6138d59370ed30
jtmer276 <159070824+jtmer276@users.noreply.github.com>
2024-04-14 17:10:40 -0400

    grgr

---
+++ TimeConversion.java
@@ -0,0 +1,28 @@
+/***
+Name: Jules Merrill
+Project: Time Conversion
+***/
+
+import java.util.*;
+
+public class TimeConversion{
+    public static void main(String[] args){
+        int mins_initial = 0;
+        int hours = 0;
+        int minutes = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the time in minutes: ");
+        mins_initial = input.nextInt();
+
+        minutes = mins_initial%60;
+        hours = (mins_initial-minutes)/60;
+
+        System.out.println("The time is: " + hours +":"+ minutes);
+
+
+        input.close();
+
+    }
+
+}
